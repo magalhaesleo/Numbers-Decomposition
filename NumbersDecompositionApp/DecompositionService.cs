eef7a740e35ccb6a6891dc7031343ce5bf3196e8
Leonardo <leonardomagalhaes_leo@hotmail.com>
2022-05-16 22:53:35 -0300

    add solution and project

---
+++ NumbersDecompositionApp/DecompositionService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NumbersDecompositionApp
+{
+    public interface IDecompositionService
+    {
+        IEnumerable<int> GetDivisorNumbers(int number);
+        IEnumerable<int> GetPrimeNumbers(IEnumerable<int> numbers);
+        bool IsPrime(int number);
+    }
+    public class DecompositionService : IDecompositionService
+    {
+        public IEnumerable<int> GetDivisorNumbers(int number)
+        {
+            for (int i = 1; i <= number; i++)
+            {
+                if (number % i == 0)
+                    yield return i;
+            }
+        }
+
+        public IEnumerable<int> GetPrimeNumbers(IEnumerable<int> numbers)
+        {
+            return numbers.Where(IsPrime);
+        }
+
+        public bool IsPrime(int number)
+        {
+            if (number < 2)
+                return false;
+
+            if (number % 2 == 0)
+                return number == 2;
+
+            int squareRoot = (int)Math.Sqrt(number);
+            for (int i = 3; i <= squareRoot; i += 2)
+            {
+                if (number % i == 0)
+                    return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ NumbersDecompositionApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace NumbersDecompositionApp
+{
+    internal static class Program
+    {
+        static void Main(string[] args)
+        {
+            int value;
+
+            while (true)
+            {
+                Console.Write("Digite um número: ");
+                var line = Console.ReadLine();
+                if (line == null || int.TryParse(line, out value) is false)
+                {
+                    Console.WriteLine("Valor inválido, tente novamente...");
+                    continue;
+                }
+
+                break;
+            }
+
+            var decomposition = new DecompositionService();
+            var divisors = decomposition.GetDivisorNumbers(value).ToList();
+            var primeNumbers = decomposition.GetPrimeNumbers(divisors).ToList();
+
+            Console.WriteLine("Número de Entrada: {0}", value);
+            Console.WriteLine("Números divisores: {0}", string.Join(' ', divisors));
+            Console.WriteLine("Dívisores Primos: {0}", string.Join(' ', primeNumbers));
+        }
+    }
+}
