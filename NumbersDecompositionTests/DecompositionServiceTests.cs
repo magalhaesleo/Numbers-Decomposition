fb8fe63d80b7abde5240d6fbc3903cbf19ee9588
Leonardo <leonardomagalhaes_leo@hotmail.com>
2022-05-16 23:06:12 -0300

    add GetPrimeNumbers unit test

--- NumbersDecompositionTests/DecompositionServiceTests.cs
+++ NumbersDecompositionTests/DecompositionServiceTests.cs
@@ -28,6 +28,20 @@ namespace NumbersDecompositionTests
             result.Should().Be(expectedResult);
         }
 
+        [Fact]
+        public void Given_an_integer_collection_should_filter_and_return_prime_numbers_only()
+        {
+            // Arrange
+            var numbers = new[] { 0, 2, 3, 4, 5, 7, 8, 11, 17, 99, 189 };
+            var expectedResult = new[] { 2, 3, 5, 7, 11, 17 };
+
+            // Act
+            var primeNumbers = _decompositionService.GetPrimeNumbers(numbers);
+
+            // Assert
+            primeNumbers.Should().BeEquivalentTo(expectedResult);
+        }
+
         public static TheoryData<int, bool> IsPrimeData()
         {
             return new TheoryData<int, bool>
